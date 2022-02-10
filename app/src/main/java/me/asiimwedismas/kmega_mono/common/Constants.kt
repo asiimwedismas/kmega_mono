f74445f0e7240fca36732b0f11227d25df526858
asiimwedismas <asiimwedismas@gmail.com>
2022-02-10 18:41:31 +0300

    add bakery collection name constants to project

--- app/src/main/java/me/asiimwedismas/kmega_mono/common/Constants.kt
+++ app/src/main/java/me/asiimwedismas/kmega_mono/common/Constants.kt
@@ -1,6 +1,15 @@
-package me.asiimwedismas.bakery_module.other
+package me.asiimwedismas.kmega_mono.common
 
 object Constants {
-
     const val LOCAL_DB = "bakery_database"
+    const val v2_BAKERY = "bakery_document"
+    const val PRODUCTION_COLLECTION = "production"
+    const val DISPATCH_COLLECTION = "dispatch"
+    const val OUTLET_DELIVERY_COLLECTION = "outlet_delivery"
+    const val AGENT_DELIVERY_COLLECTION = "agent_delivery"
+    const val RETURNS_COLLECTION = "returns"
+    const val EXPIRED_COLLECTION = "expired"
+    const val AUDIT_COLLECTION = "audit"
+
+    const val v1_BAKERY = "v1_bakery_document"
 }
